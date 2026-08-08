0639633451be2a66dd61f5902acd69de205019c5
Carlos Chavez <carloschave_2003_07@hotmail.com>
2026-08-07 20:30:21 -0500

    Primer Push-Proyecto

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace EcoPredict_NLQ.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
